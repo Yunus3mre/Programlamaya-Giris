f14ca4f4316f8d2f5f8e73785205af319cbbb2e5
Yunus3mre <45149807+Yunus3mre@users.noreply.github.com>
2019-04-16 12:13:42 +0300

    Update MerhabaDunya.java

--- MerhabaDunya.java
+++ MerhabaDunya.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class MerhabaDuna{
   public static void main(String args[]){
-    System.out.println("Merahba Dunya");
+  System.out.println("sayı girin:");
+	Scanner tarayıcı=new Scanner(System.in);
+	int sayı=tarayıcı.nextInt();
+	
+	for(int i=sayı-1;i>=0;i--) {
+		for(int j=0;j<=sayı-1;j++) {
+			if(i==j) {
+				System.out.print(1+"\t");
+			}
+			else if(i!=j) {
+				System.out.print(0+"\t");
+			}
+			
+		}
+		System.out.println("");
+	}
   }
 }
